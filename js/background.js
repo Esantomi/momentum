6dd8ac14e713018d8d5aed4fe319ef11e263d693
Esantomi <pilinnasa@gmail.com>
2021-09-05 21:18:55 +0900

    almost finished

--- js/background.js
+++ js/background.js
@@ -5,7 +5,10 @@
 const images = [
     "0.jpg",
     "1.jpg",
-    "2.jpg"
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg"
 ]
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
@@ -13,7 +16,7 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 // document.createElement() : html element 생성
 const bgImage = document.createElement("img");
-// console.log(bgImage);  // img 객체(=== <img>) 출력 
+console.log(bgImage);  // img 객체(=== <img>) 출력 
 
 bgImage.src = `img/${chosenImage}`;
 // console.log(bgImage);  // <img src="img/*.jpg">
